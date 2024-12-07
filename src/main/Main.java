69430e2fe486bcb34bba7837fecfae8949d675c5
unknown <burakgulduz@gmail.com>
2024-12-07 15:27:34 +0100

    Connection to API. DONT UPLOAD TOKEN TO GITgit add *git add *

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,50 @@
-package src.main.java.com.uas.dronesim;
+package src.main;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test");
+import javax.swing.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import  java.net.URL;
+import java.net.HttpURLConnection;
+
+public class Main
+{
+    // DONT UPLOAD TOKEN TO GIT!!!!!
+    private static final String TOKEN = "";
+    private static final String ENDPOINT_URL = "http://dronesim.facets-labs.com/api/";
+    private static final String USER_AGENT = "group21";
+
+    public static void main(String[] args)
+    {
+        try {
+            URL url = new URL(ENDPOINT_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", "Token " + TOKEN);
+            conn.setRequestProperty("User-Agent", USER_AGENT);
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Connection established.");
+            } else {
+                System.out.print("Error Details: ");
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                String inputLine;
+                while ( (inputLine = in.readLine()) != null ) {
+                    System.out.println(inputLine);
+                }
+                in.close();
+
+            }
+
+            conn.disconnect();
+        } catch (Exception e) {
+            System.out.print("");
+        }
+
     }
 }
