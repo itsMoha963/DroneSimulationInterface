1e4f3239934c0fd963156768d077ac20b6f750d2
Shaydur Sarker <shaydursarker@gmail.com>
2025-01-27 19:43:19 +0100

    Added Descriptions to ComponentAdapter

--- src/main/java/services/DroneFilterService.java
+++ src/main/java/services/DroneFilterService.java
@@ -7,6 +7,14 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Filters drones using condition
+ * @param drones The collection of drones
+ * @param filter which condition to apply
+ * @param <T> Type of drones, extends DroneBase
+ * @param return List of drones meeting the conditions
+ */
+
 public class DroneFilterService {
 
     public <T extends DroneBase> List<T> filterDrones(Collection<T> drones, BaseDroneFilter<T> filter) {
@@ -14,6 +22,14 @@ public class DroneFilterService {
                 .filter(drone -> meetsCondition(drone, filter))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * Checks if a drone meets the conditions
+     * @param drone The drone that is currently checked
+     * @param filter The condition to check with
+     * @param <T> Type of drone
+     * @return set to true if condition are met
+     */
 
     private <T extends DroneBase> boolean meetsCondition(T drone, BaseDroneFilter<T> filter) {
         return filter.getConditions().stream()
